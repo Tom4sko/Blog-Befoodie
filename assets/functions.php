1b8724d192ff7ad5475a01332196ed9d2357e6b2
Tomáš Zeleňák <108126659+Tom4sko@users.noreply.github.com>
2024-03-24 15:20:59 +0100

    Added functions for greetings, banner changes. Added components and connected them

---
+++ assets/functions.php
@@ -0,0 +1,39 @@
+<?php
+
+function pridajPozdrav() {
+    $hour = date(format: 'H'); // date_default_timezone_set()
+
+    if ($hour < 12) {
+        echo "<h3>Good Morning</h3>";
+    } elseif ($hour < 18) {
+        echo "<h3>Good Day</h3>";
+    } else {
+        echo "<h3>Good Evening</h3>";
+    }
+}
+
+
+function generateSlides($dir) {
+    $files = glob($dir . "/*.jpg");
+    $json = file_get_contents("database/data.json");
+    $data = json_decode($json, true);
+    
+    foreach ($files as $file) {
+        echo '<div class="slide fade">';
+        echo '<img src="' . $file . '">';
+        $filename = basename($file); // Získání názvu souboru
+        // Získání textu k danému banneru
+        if (isset($data["banner"][$filename])) {
+            echo '<div class="slide-text">';
+            echo $data["banner"][$filename]; // Opraveno z $data["text_banner"][$filename]
+            echo '</div>';
+        } else {
+            echo '<div class="slide-text">No text available for this banner</div>';
+        }
+        echo '</div>';
+    }
+}
+
+
+
+?>
---
+++ components/navbar.php
@@ -0,0 +1,18 @@
+<header class="container main-header">
+    <div>
+        <a href="index.php">
+        <img src="img/logo.png" height="40">
+        </a>
+    </div>
+    <nav class="main-nav">
+    <ul class="main-menu" id="main-menu">
+        <li><a href="index.php">Domov</a></li>
+        <li><a href="portfolio.php">Portfólio</a></li>
+        <li><a href="qna.php">Q&A</a></li>
+        <li><a href="kontakt.php">Kontakt</a></li>
+    </ul>
+    <a class="hamburger" id="hamburger">
+        <i class="fa fa-bars"></i>
+    </a>
+    </nav>
+</header>
